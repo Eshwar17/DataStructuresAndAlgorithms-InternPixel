ead986ce685a0bf232ae163d298a6ba309c0cf1b
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-11 13:24:37 +0530

    Created a function insertAtHead which can insert a node at head

--- LinkedLists/LinkedListNode.cpp
+++ LinkedLists/LinkedListNode.cpp
@@ -11,6 +11,14 @@ class Node{
     }
 };
 
+//Insertion at head where head's reference is passed as a parameter so that we can avoid creating new node instead we can take the copy of it
+void insertAtHead(Node* &head, int d) {
+    //New node creation
+    Node* newNode = new Node(d);
+    newNode -> next = head;
+    head = newNode;
+}
+
 int main(){
     Node* node1 = new Node(10);
     cout << node1 -> data << endl;
